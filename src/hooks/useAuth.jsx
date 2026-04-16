2bcc8bc435f86005ad87ae48929d53d5a43e1e4d
Nurlianto Aldi <nurliantoaldi@gmail.com>
2026-04-16 12:38:56 +0700

    Fix bug at useAuth.jsx

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function useAuth() {
   const [isLoading, setIsLoading] = useState(false);
